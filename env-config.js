f762283e026bc8f468e7421912ce503852da69d5
Somes Kumar K <someshkumarasamy@gmail.com>
2020-07-12 17:38:56 +0530

    vercel env try 1

--- env-config.js
+++ env-config.js
@@ -2,5 +2,6 @@ const prod = process.env.NODE_ENV === 'production';
 
 module.exports ={
     'process.env.APPID' : 'urlshortner',
-    'process.env.BASE_URL':prod?'http://localhost:3000/':''
+    'process.env.BASE_URL':prod?'http://localhost:3000/':'https://url-shortner-apympxbwp.vercel.app/',
+    'process.env.DB_URI': prod?'':'mongodb+srv://test:Pass12345@cluster0-aw7ut.mongodb.net/test?retryWrites=true&w=majority'
 }
